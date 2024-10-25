40c9a44195b63e1996862dc0796f366d1533efc9
Min Cheol Song <sendol39@gmail.com>
2024-10-25 08:36:44 +0000

    Finished api

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,28 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
+
   let convertHandler = new ConvertHandler();
+  app.route('/api/convert').get((req, res) => {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+    if (!initNum && !initUnit) {
+      // res.statusCode = 400;
+      return res.json({ error: 'invalid number and unit' });
+    }
+    if (!initNum) {
+      // res.statusCode = 400;
+      return res.json({ error: 'invalid number' });
+    }
+    if (!initUnit) {
+      // res.statusCode = 400;
+      return res.json({ error: 'invalid unit' });
+    }
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+    res.json({ initNum, initUnit, returnNum, returnUnit, string: toString });
+  });
 
 };
